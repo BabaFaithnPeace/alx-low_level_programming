06a8b65166ac42795879f404046ec21890515d38
BabaFaithnPeace <kiogorainvs@gmail.com>
2023-03-01 04:45:22 -0800

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - this is the main function
+  *
+  *Return: this value is always 0
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
